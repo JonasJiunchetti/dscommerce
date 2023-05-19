166cc0ebf8911d24317589979867157c1dfd92ac
Jonas <jonas.jiunchetti@outlook.com>
2023-05-19 15:13:02 -0300

    Entities User, Order, OrderStatus and data H2 database

---
+++ src/main/java/com/projectcommerce/dscommerce/Entities/Order.java
@@ -0,0 +1,36 @@
+package com.projectcommerce.dscommerce.Entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_order")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Order {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+}
---
+++ src/main/java/com/projectcommerce/dscommerce/Entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.projectcommerce.dscommerce.Entities;
+
+public enum OrderStatus {
+    
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}

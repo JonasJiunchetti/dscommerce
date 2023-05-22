37bc9e0879d83219559c3daa8bf1acd8d4c9a385
Jonas <jonas.jiunchetti@outlook.com>
2023-05-22 14:07:27 -0300

    OrderItem

---
+++ src/main/java/com/projectcommerce/dscommerce/Entities/OrderItemPK.java
@@ -0,0 +1,21 @@
+package com.projectcommerce.dscommerce.Entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@Data
+public class OrderItemPK {
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}

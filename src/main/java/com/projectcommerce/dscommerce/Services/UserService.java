cf41af17fd146cfe4ced740b73edd00dd59053b3
Jonas J <jonas.jiunchetti@outlook.com>
2023-08-05 11:59:47 -0300

    Get User logged

--- src/main/java/com/projectcommerce/dscommerce/Services/UserService.java
+++ src/main/java/com/projectcommerce/dscommerce/Services/UserService.java
@@ -3,11 +3,16 @@ package com.projectcommerce.dscommerce.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.projectcommerce.dscommerce.DTO.UserDTO;
 import com.projectcommerce.dscommerce.entities.Role;
 import com.projectcommerce.dscommerce.entities.User;
 import com.projectcommerce.dscommerce.projections.UserDetailsProjection;
@@ -34,5 +39,23 @@ public class UserService implements UserDetailsService {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return repository.findByEmail(username).get();
+        }
+        catch(Exception e){
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
